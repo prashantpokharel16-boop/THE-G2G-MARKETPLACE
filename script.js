4c364069c98b836c56681d282fb0202c82a38d21
G2G <prashantpokharel16@gmail.com>
2026-05-28 16:39:05 +0545

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ function loadReviewsFromStorage() {
 
     let defaultHTML = `
         <div class="review-box-p">
+
             <div class="r-top">
+
                 <span class="r-user">
                     <i class="fa-solid fa-circle-user"></i>
                     AlphaX
                 </span>
 
-                <span class="r-stars">⭐⭐⭐⭐⭐</span>
+                <span class="r-stars">
+                    ⭐⭐⭐⭐⭐
+                </span>
+
             </div>
 
             <p>
@@ -24,6 +29,7 @@ function loadReviewsFromStorage() {
                 <i class="fa-solid fa-clock"></i>
                 Verified Review
             </span>
+
         </div>
     `;
 
@@ -33,39 +39,95 @@ function loadReviewsFromStorage() {
     globalReviews.forEach(rev => {
 
         let author = rev.author || "Unknown User";
+
         let stars = rev.stars || "⭐⭐⭐⭐⭐";
+
         let text = rev.text || "No review text";
 
-        defaultHTML =
-        `
+        defaultHTML = `
+
         <div class="review-box-p custom-user-review">
 
             <div class="r-top">
 
                 <span class="r-user" style="color:#3b82f6;">
+
                     <i class="fa-solid fa-circle-user text-blue"></i>
+
                     ${author}
+
                 </span>
 
                 <span class="r-stars">
+
                     ${stars}
+
                 </span>
 
             </div>
 
             <p>
+
                 "${text}"
+
             </p>
 
             <span class="r-date">
+
                 <i class="fa-solid fa-circle-nodes"></i>
+
                 Live Public Contribution
+
             </span>
 
         </div>
+
         ` + defaultHTML;
 
     });
 
     reviewContainer.innerHTML = defaultHTML;
+}
+
+
+function submitLiveReview(e) {
+
+    e.preventDefault();
+
+    const starsSelection =
+        document.getElementById("review-stars-select").value;
+
+    const reviewTextContent =
+        document.getElementById("review-text-input").value.trim();
+
+    if (!reviewTextContent) {
+
+        alert("Please enter a review.");
+
+        return;
+    }
+
+    let globalReviews =
+        JSON.parse(localStorage.getItem("publicReviewsArray")) || [];
+
+    const newReviewPacket = {
+
+        author: currentUser || "Anonymous",
+
+        stars: starsSelection || "⭐⭐⭐⭐⭐",
+
+        text: reviewTextContent || "No review"
+
+    };
+
+    globalReviews.push(newReviewPacket);
+
+    localStorage.setItem(
+        "publicReviewsArray",
+        JSON.stringify(globalReviews)
+    );
+
+    document.getElementById("review-text-input").value = "";
+
+    loadReviewsFromStorage();
 }
